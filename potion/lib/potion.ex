34ccc97377b001ee92a76172912b2ef7b5ae1af4
Carl Hickman <chickman@c02t21n9gtdy.myfamily.int>
2019-08-26 08:50:50 -0600

    files

---
+++ potion/lib/potion.ex
@@ -0,0 +1,18 @@
+defmodule Potion do
+  @moduledoc """
+  Documentation for Potion.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Potion.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
